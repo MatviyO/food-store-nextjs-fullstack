ebaeeb1e849b9bbf3796902bcbc0788198be9baa
Matvii Oleh <oleh.matvii@bitsorchestra.com>
2024-09-22 13:34:32 +0300

    feat: eslint

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:@next/next/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'react'],
+  rules: {
+    'indent': ['error', 2],
+    'react/jsx-indent': ['error', 2],
+    'react/jsx-indent-props': ['error', 2],
+    'react/react-in-jsx-scope': 'off',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
---
+++ app/product/[id]/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function ProductPage() {
+  return (
+    <div className="grid">
+      <h1>Product</h1>
+    </div>
+  );
+}
