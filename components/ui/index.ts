19a5702659eed888f5baba34c137c91145feb21f
Matvii Oleh <oleh.matvii@bitsorchestra.com>
2024-09-23 21:16:31 +0300

    feat: header

---
+++ components/ui/index.ts
@@ -0,0 +1,9 @@
+export { Button} from './button';
+export { Input } from './input';
+export { Label } from './label';
+export { Checkbox } from './checkbox';
+export { Slider } from './slider';
+export { Tooltip } from './tooltip';
+export { Popover } from './popover';
+export { Dialog } from './dialog';
+export { Skeleton } from './skeleton';
