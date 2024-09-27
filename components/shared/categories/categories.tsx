b6023ad4c8121c5c88cc466083f58f6a663079bb
Matvii Oleh <oleh.matvii@bitsorchestra.com>
2024-09-27 22:26:00 +0300

    feat: categories

--- components/shared/categories/categories.tsx
+++ components/shared/categories/categories.tsx
@@ -1,6 +1,7 @@
 "use client";
 import React, {FC} from "react";
 import { cn } from "@/lib/utils";
+import {useCategoryStore} from "@/store/category";
 // import { useCategoryStore } from '@/store/category';
 // import { Category } from '@prisma/client';
 
@@ -10,7 +11,7 @@ interface Props {
 }
 
 export const Categories: FC<Props> = ({ items, className }) => {
-  // const activeId = useCategoryStore((state) => state.activeId);
+  const activeId = useCategoryStore((state) => state.activeId);
 
   return (
     <div className={cn("inline-flex gap-1 bg-gray-50 p-1 rounded-2xl", className)}>
@@ -19,7 +20,7 @@ export const Categories: FC<Props> = ({ items, className }) => {
           key={category.id}
           className={cn(
             "flex items-center font-bold h-11 rounded-2xl px-5",
-            // activeId === category.id && 'bg-white shadow-md shadow-gray-200 text-primary',
+            activeId === category.id && 'bg-white shadow-md shadow-gray-200 text-primary',
           )}
           href={`/#${category.name}`}>
           {category.name}
