027001d10ea73f1e5d91323e5cb048a00755e069
Matvii Oleh <oleh.matvii@bitsorchestra.com>
2024-10-08 22:41:21 +0300

    feat: mockup

--- prisma/constants.ts
+++ prisma/constants.ts
@@ -1,3 +1,5 @@
+import { hashSync } from "bcrypt";
+
 export const categoriesSeed = [
   {
     name: 'Pizzas',
@@ -16,9 +18,9 @@ export const categoriesSeed = [
   },
 ];
 
-export const ingredientsSeed = [
+export const ingredients = [
   {
-    name: 'Cheese-Stuffed Crust',
+    name: 'Cheese Crust',
     price: 179,
     imageUrl:
       'https://cdn.dodostatic.net/static/Img/Ingredients/99f5cb91225b4875bd06a26d2e842106.png',
@@ -30,12 +32,12 @@ export const ingredientsSeed = [
       'https://cdn.dodostatic.net/static/Img/Ingredients/cdea869ef287426386ed634e6099a5ba.png',
   },
   {
-    name: 'Cheddar and Parmesan Cheeses',
+    name: 'Cheddar and Parmesan Cheese',
     price: 79,
     imageUrl: 'https://cdn.dodostatic.net/static/Img/Ingredients/000D3A22FA54A81411E9AFA69C1FE796',
   },
   {
-    name: 'Spicy Jalapeño Peppers',
+    name: 'Spicy Jalapeño Pepper',
     price: 59,
     imageUrl:
       'https://cdn.dodostatic.net/static/Img/Ingredients/11ee95b6bfdf98fb88a113db92d7b3df.png',
@@ -81,7 +83,7 @@ export const ingredientsSeed = [
     imageUrl: 'https://cdn.dodostatic.net/static/Img/Ingredients/000D3A22FA54A81411E9AFA60AE6464C',
   },
   {
-    name: 'Juicy Pineapples',
+    name: 'Juicy Pineapple',
     price: 59,
     imageUrl: 'https://cdn.dodostatic.net/static/Img/Ingredients/000D3A21DA51A81211E9AFA6795BA2A0',
   },
@@ -92,7 +94,7 @@ export const ingredientsSeed = [
       'https://cdn.dodostatic.net/static/Img/Ingredients/370dac9ed21e4bffaf9bc2618d258734.png',
   },
   {
-    name: 'Sweet Peppers',
+    name: 'Bell Pepper',
     price: 59,
     imageUrl: 'https://cdn.dodostatic.net/static/Img/Ingredients/000D3A22FA54A81411E9AFA63F774C1B',
   },
@@ -107,7 +109,7 @@ export const ingredientsSeed = [
     imageUrl:
       'https://cdn.dodostatic.net/static/Img/Ingredients/b2f3a5d5afe44516a93cfc0d2ee60088.png',
   },
-];
+].map((obj, index) => ({ id: index + 1, ...obj }));
 
 export const productsSeed = [
   {
@@ -197,3 +199,179 @@ export const productsSeed = [
   },
 ]
 
+export const products = [
+  {
+    name: 'Omelet with Ham and Mushrooms',
+    imageUrl:
+      'https://media.dodostatic.net/image/r:292x292/11EE7970321044479C1D1085457A36EB.webp',
+    categoryId: 2,
+  },
+  {
+    name: 'Omelet with Pepperoni',
+    imageUrl:
+      'https://media.dodostatic.net/image/r:292x292/11EE94ECF33B0C46BA410DEC1B1DD6F8.webp',
+    categoryId: 2,
+  },
+  {
+    name: 'Café Latte',
+    imageUrl:
+      'https://media.dodostatic.net/image/r:292x292/11EE7D61B0C26A3F85D97A78FEEE00AD.webp',
+    categoryId: 2,
+  },
+  {
+    name: 'Ham and Cheese Sandwich',
+    imageUrl:
+      'https://media.dodostatic.net/image/r:292x292/11EE796FF0059B799A17F57A9E64C725.webp',
+    categoryId: 3,
+  },
+  {
+    name: 'Chicken Nuggets',
+    imageUrl:
+      'https://media.dodostatic.net/image/r:292x292/11EE7D618B5C7EC29350069AE9532C6E.webp',
+    categoryId: 3,
+  },
+  {
+    name: 'Baked Potatoes with Sauce 🌱',
+    imageUrl:
+      'https://media.dodostatic.net/image/r:292x292/11EED646A9CD324C962C6BEA78124F19.webp',
+    categoryId: 3,
+  },
+  {
+    name: 'Dodster',
+    imageUrl:
+      'https://media.dodostatic.net/image/r:292x292/11EE796F96D11392A2F6DD73599921B9.webp',
+    categoryId: 3,
+  },
+  {
+    name: 'Spicy Dodster 🌶️🌶️',
+    imageUrl:
+      'https://media.dodostatic.net/image/r:292x292/11EE796FD3B594068F7A752DF8161D04.webp',
+    categoryId: 3,
+  },
+  {
+    name: 'Banana Milkshake',
+    imageUrl:
+      'https://media.dodostatic.net/image/r:292x292/11EEE20B8772A72A9B60CFB20012C185.webp',
+    categoryId: 4,
+  },
+  {
+    name: 'Caramel Apple Milkshake',
+    imageUrl:
+      'https://media.dodostatic.net/image/r:292x292/11EE79702E2A22E693D96133906FB1B8.webp',
+    categoryId: 4,
+  },
+  {
+    name: 'Oreo Cookie Milkshake',
+    imageUrl:
+      'https://media.dodostatic.net/image/r:292x292/11EE796FA1F50F8F8111A399E4C1A1E3.webp',
+    categoryId: 4,
+  },
+  {
+    name: 'Classic Milkshake 👶',
+    imageUrl:
+      'https://media.dodostatic.net/image/r:292x292/11EE796F93FB126693F96CB1D3E403FB.webp',
+    categoryId: 4,
+  },
+  {
+    name: 'Irish Cappuccino',
+    imageUrl:
+      'https://media.dodostatic.net/image/r:292x292/11EE7D61999EBDA59C10E216430A6093.webp',
+    categoryId: 5,
+  },
+  {
+    name: 'Caramel Cappuccino',
+    imageUrl:
+      'https://media.dodostatic.net/image/r:292x292/11EE7D61AED6B6D4BFDAD4E58D76CF56.webp',
+    categoryId: 5,
+  },
+  {
+    name: 'Coconut Latte',
+    imageUrl:
+      'https://media.dodostatic.net/image/r:292x292/11EE7D61B19FA07090EE88B0ED347F42.webp',
+    categoryId: 5,
+  },
+  {
+    name: 'Americano Coffee',
+    imageUrl:
+      'https://media.dodostatic.net/image/r:292x292/11EE7D61B044583596548A59078BBD33.webp',
+    categoryId: 5,
+  },
+  {
+    name: 'Café Latte',
+    imageUrl:
+      'https://media.dodostatic.net/image/r:292x292/11EE7D61B0C26A3F85D97A78FEEE00AD.webp',
+    categoryId: 5,
+  },
+];
+
+export const stories = [
+  {
+    storyId: 1,
+    sourceUrl:
+      'https://cdn.inappstory.ru/file/dd/yj/sx/oqx9feuljibke3mknab7ilb35t.webp?k=IgAAAAAAAAAE',
+  },
+  {
+    storyId: 1,
+    sourceUrl:
+      'https://cdn.inappstory.ru/file/jv/sb/fh/io7c5zarojdm7eus0trn7czdet.webp?k=IgAAAAAAAAAE',
+  },
+  {
+    storyId: 1,
+    sourceUrl:
+      'https://cdn.inappstory.ru/file/ts/p9/vq/zktyxdxnjqbzufonxd8ffk44cb.webp?k=IgAAAAAAAAAE',
+  },
+  {
+    storyId: 1,
+    sourceUrl:
+      'https://cdn.inappstory.ru/file/ur/uq/le/9ufzwtpdjeekidqq04alfnxvu2.webp?k=IgAAAAAAAAAE',
+  },
+  {
+    storyId: 1,
+    sourceUrl:
+      'https://cdn.inappstory.ru/file/sy/vl/c7/uyqzmdojadcbw7o0a35ojxlcul.webp?k=IgAAAAAAAAAE',
+  },
+]
+
+export const storiesSeed = [
+  {
+    previewImageUrl:
+      'https://cdn.inappstory.ru/story/xep/xzh/zmc/cr4gcw0aselwvf628pbmj3j/custom_cover/logo-350x440.webp?k=IgAAAAAAAAAE&v=3101815496',
+  },
+  {
+    previewImageUrl:
+      'https://cdn.inappstory.ru/story/km2/9gf/jrn/sb7ls1yj9fe5bwvuwgym73e/custom_cover/logo-350x440.webp?k=IgAAAAAAAAAE&v=3074015640',
+  },
+  {
+    previewImageUrl:
+      'https://cdn.inappstory.ru/story/quw/acz/zf5/zu37vankpngyccqvgzbohj1/custom_cover/logo-350x440.webp?k=IgAAAAAAAAAE&v=1336215020',
+  },
+  {
+    previewImageUrl:
+      'https://cdn.inappstory.ru/story/7oc/5nf/ipn/oznceu2ywv82tdlnpwriyrq/custom_cover/logo-350x440.webp?k=IgAAAAAAAAAE&v=38903958',
+  },
+  {
+    previewImageUrl:
+      'https://cdn.inappstory.ru/story/q0t/flg/0ph/xt67uw7kgqe9bag7spwkkyw/custom_cover/logo-350x440.webp?k=IgAAAAAAAAAE&v=2941222737',
+  },
+  {
+    previewImageUrl:
+      'https://cdn.inappstory.ru/story/lza/rsp/2gc/xrar8zdspl4saq4uajmso38/custom_cover/logo-350x440.webp?k=IgAAAAAAAAAE&v=4207486284',
+  },
+]
+
+export const users = [
+  {
+    fullName: 'User',
+    email: 'user@test.com',
+    password: hashSync('111111', 10),
+    verified: new Date(),
+    role: 'USER',
+  },
+  {
+    fullName: 'Admin',
+    email: 'admin@test.com',
+    password: hashSync('111111', 10),
+    verified: new Date(),
+    role: 'ADMIN',
+  },
+] as any[];
