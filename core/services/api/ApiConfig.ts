143507d0ae5bfdd3a7e445f8213ba4959b003752
Matvii Oleh <oleh.matvii@bitsorchestra.com>
2024-10-10 22:31:53 +0300

    feat: axios

---
+++ core/services/api/ApiConfig.ts
@@ -0,0 +1,10 @@
+
+export const ApiConfig = {
+  baseUrl: process.env.NEXT_PUBLIC_API_URL,
+  timeout: 5000
+}
+
+export const ApiUrl = {
+  search: '/products/search',
+  ingredients: '/ingredients'
+}
