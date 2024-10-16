0f398668a8999d9a504e5a1eeab7ebfb9cadc615
Matvii Oleh <oleh.matvii@bitsorchestra.com>
2024-10-16 23:24:44 +0300

    feat: filter

---
+++ constants/filterPizza.ts
@@ -0,0 +1,15 @@
+const filterPizza = [
+  { text: "20 cm", value: "20" },
+  { text: "30 cm", value: "30" },
+  { text: "40 cm", value: "40" },
+];
+
+
+const filterTypesPizza = [
+  { text: "Thin Crust", value: "1" },
+  { text: "Traditional", value: "2" },
+]
+export {
+  filterPizza,
+  filterTypesPizza,
+}
