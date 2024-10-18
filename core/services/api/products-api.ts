ae1d5ee5427f4c309b10d55d3921b6f7f2f58aae
Matvii Oleh <oleh.matvii@bitsorchestra.com>
2024-10-18 19:36:12 +0300

    feat: api

---
+++ core/services/api/products-api.ts
@@ -0,0 +1,9 @@
+import { Product } from '@prisma/client';
+import { axiosInstance } from "@/core/services/api/axios";
+import {ApiUrl} from "@/core/services/api/ApiConfig";
+
+export const search = async (query: string) => {
+  const { data } = await axiosInstance.get<Product[]>(ApiUrl.search, { params: { query } });
+
+  return data;
+};
