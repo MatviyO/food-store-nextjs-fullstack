f8cbbcfa428d785de0d20749b9905d5b5e9ae1c6
Matvii Oleh <oleh.matvii@bitsorchestra.com>
2024-10-21 21:40:23 +0300

    feat: client

--- core/services/api/api-client.ts
+++ core/services/api/api-client.ts
@@ -1,7 +1,13 @@
 import * as products from './products-api';
 import * as ingredients from './ingredients-api';
+import * as stories from './stories';
+import * as auth from './auth-api';
+import * as cart from './cart-api';
 
 export const Api = {
   products,
   ingredients,
+  stories,
+  cart,
+  auth,
 };
