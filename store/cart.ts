26d0f4ea79fef8e34324e2dc63da1b8a65fc42c6
Matvii Oleh <oleh.m97@gmail.com>
2024-10-24 19:57:06 +0300

    feat: cart

---
+++ store/cart.ts
@@ -0,0 +1,81 @@
+import { create } from 'zustand';
+import { CreateCartItemValues } from "@/core/types/dto/cart";
+import { Api } from "@/core/services/api/api-client";
+import { getCartDetails } from "@/shared/libs/get-cart-details";
+
+export type ICartItem = {
+  id: number;
+  quantity: number;
+  name: string;
+  imageUrl: string;
+  price: number;
+  pizzaSize?: number | null;
+  type?: number | null;
+  ingredients: Array<{ name: string; price: number }>;
+};
+
+export interface CartState {
+  loading: boolean;
+  error: boolean;
+  totalAmount: number;
+  items: ICartItem[];
+  fetchCartItems: () => Promise<void>;
+  updateItemQuantity: (id: number, quantity: number) => Promise<void>;
+  addCartItem: (values: CreateCartItemValues) => Promise<void>;
+  removeCartItem: (id: number) => Promise<void>;
+}
+
+export const useCartStore = create<CartState>((set, get) => ({
+  items: [],
+  error: false,
+  loading: true,
+  totalAmount: 0,
+  removeCartItem: async (id: number) => {
+    try {
+      set({ loading: true, error: false });
+      const data = await Api.cart.removeCartItem(id);
+      set(getCartDetails(data));
+    } catch (error) {
+      set({ error: true, loading: false });
+      console.error(error);
+    } finally {
+      set({ loading: false });
+    }
+  },
+  fetchCartItems: async () => {
+    try {
+      set({ loading: true, error: false });
+      const data = await Api.cart.fetchCart();
+      set(getCartDetails(data));
+    } catch (error) {
+      console.error(error);
+      set({ error: true, loading: false });
+    } finally {
+      set({ loading: false });
+    }
+  },
+  updateItemQuantity: async (id: number, quantity: number) => {
+    try {
+      set({ loading: true, error: false });
+      const data = await Api.cart.updateItemQuantity(id, quantity);
+      set(getCartDetails(data));
+    } catch (error) {
+      console.error(error);
+      set({ error: true, loading: false });
+    } finally {
+      set({ loading: false });
+    }
+  },
+  addCartItem: async (values: CreateCartItemValues) => {
+    try {
+      set({ loading: true, error: false });
+      const data = await Api.cart.addCartItem(values);
+      set(getCartDetails(data));
+    } catch (error) {
+      console.error(error);
+      set({ error: true, loading: false });
+    } finally {
+      set({ loading: false });
+    }
+  },
+}));
