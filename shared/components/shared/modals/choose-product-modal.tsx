8ba9f92a6d00f853bd5a58cf2a3c911ed9d6ea2d
Matvii Oleh <oleh.m97@gmail.com>
2024-10-29 00:00:22 +0200

    feat: checkbox

---
+++ shared/components/shared/modals/choose-product-modal.tsx
@@ -0,0 +1,52 @@
+'use client';
+
+import React, { FC } from 'react';
+import { useRouter } from 'next/navigation';
+import { IProduct } from "@/shared/hooks/common/card/use-choose-pizza";
+import {
+  ChoosePizzaForm
+} from "@/shared/components/shared/choose-form/choose-pizza-form";
+import {
+  ChooseProductForm
+} from "@/shared/components/shared/choose-form/choose-product-form";
+import { Dialog } from "@/shared/components/ui";
+import { DialogContent } from "@/shared/components/ui/dialog";
+
+interface Props {
+  product: IProduct;
+}
+
+export const ChooseProductModal: FC<Props> = ({ product }) => {
+  const router = useRouter();
+  const isPizzaForm = Boolean(product?.items[0]?.pizzaType);
+
+  const onCloseModal = () => {
+    router.back();
+  };
+
+  return (
+    <Dialog
+      open={Boolean(product)}
+      onOpenChange={onCloseModal}
+    >
+      <DialogContent className="p-0 w-[1060px] max-w-[1060px] min-h-[500px] bg-white overflow-hidden">
+        {isPizzaForm ? (
+          <ChoosePizzaForm
+            imageUrl={product.imageUrl}
+            name={product.name}
+            items={product.items}
+            onClickAdd={onCloseModal}
+            ingredients={product.ingredients}
+          />
+        ) : (
+          <ChooseProductForm
+            imageUrl={product.imageUrl}
+            name={product.name}
+            items={product.items}
+            onClickAdd={onCloseModal}
+          />
+        )}
+      </DialogContent>
+    </Dialog>
+  );
+};
