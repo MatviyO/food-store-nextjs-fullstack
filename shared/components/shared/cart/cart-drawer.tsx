30ffd3c726a68f3d558111f63aed9242e9f0f3d1
Matvii Oleh <oleh.m97@gmail.com>
2024-11-07 20:20:40 +0200

    feat: cart

--- shared/components/shared/cart/cart-drawer.tsx
+++ shared/components/shared/cart/cart-drawer.tsx
@@ -99,7 +99,7 @@ export const CartDrawer: FC<PropsWithChildren> = ({ children }) => {
                       <div className="flex-1 border-b border-dashed border-b-neutral-200 relative -top-1 mx-2" />
                     </span>
 
-                    <span className="font-bold text-lg">{totalAmount} ₽</span>
+                    <span className="font-bold text-lg">{totalAmount} $</span>
                   </div>
 
                   <Link href="/cart">
