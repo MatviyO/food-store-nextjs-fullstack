049e132849d49a5a3f2e07e7ee13b8ed6f297348
Matvii Oleh <oleh.m97@gmail.com>
2024-11-18 22:55:02 +0200

    feat: ingre component

--- core/services/api/ingredients-api.ts
+++ core/services/api/ingredients-api.ts
@@ -3,7 +3,8 @@ import { axiosInstance } from "@/core/services/api/axios";
 import { ApiUrl } from "@/core/services/api/ApiConfig";
 
 export const getAll = async () => {
-  const { data } = await axiosInstance.get<Ingredient[]>(ApiUrl.ingredients);
+  const { data } =
+    await axiosInstance.get<{ result: Ingredient[] }>(ApiUrl.ingredients);
 
-  return data;
+  return data.result;
 };
