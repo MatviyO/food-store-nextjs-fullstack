72e7c1f29cbb8f30df8f757b734af274b4732bbb
Matvii Oleh <oleh.m97@gmail.com>
2024-11-18 22:53:12 +0200

    feat: select component

--- shared/libs/get-cart-details.ts
+++ shared/libs/get-cart-details.ts
@@ -1,6 +1,8 @@
 import { ICartItem } from '@/store/cart';
 import { CartResponse } from "@/core/types/dto/cart";
 import { Ingredient, ProductItem } from "@prisma/client";
+import { DefaultSession, getServerSession } from "next-auth";
+import { authOptions } from "@/shared/constants/authOptions";
 
 type ReturnProps = {
   items: ICartItem[];
@@ -23,7 +25,7 @@ export const calcCartItemTotalAmount = (item: Item): number => {
 
 
 export const getCartDetails = (data: CartResponse): ReturnProps => {
-  const items = data.items.map((item) => ({
+  const items = data.items?.map((item) => ({
     id: item.id,
     quantity: item.quantity,
     name: item.productItem.product.name,
@@ -36,6 +38,15 @@ export const getCartDetails = (data: CartResponse): ReturnProps => {
       price: ingredient.price,
     })),
   }));
+  const totalAmount =
+    items.reduce((sum, item) => sum + item.price * item.quantity, 0);
+  return { items, totalAmount };
+};
 
-  return { items, totalAmount: data.totalAmount || 0 };
+export const getUserSession = async (): Promise<DefaultSession & {
+  id?: string
+}> => {
+  const session = await getServerSession(authOptions);
+
+  return session?.user as DefaultSession ?? null;
 };
