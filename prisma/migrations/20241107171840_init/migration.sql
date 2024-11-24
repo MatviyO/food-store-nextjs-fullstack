c69cc0e53dae240632ca85564ebbcf2d2b8527ee
Matvii Oleh <oleh.m97@gmail.com>
2024-11-24 23:31:42 +0200

    feat: ingre component

--- prisma/migrations/20241107171840_init/migration.sql
+++ prisma/migrations/20241107171840_init/migration.sql
@@ -24,8 +24,6 @@ CREATE TABLE "User" (
 CREATE TABLE "Category" (
     "id" SERIAL NOT NULL,
     "name" TEXT NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL,
 
     CONSTRAINT "Category_pkey" PRIMARY KEY ("id")
 );
