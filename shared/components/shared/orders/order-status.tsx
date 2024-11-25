c91b4f7794c22f158496037077bbfa7fc12f0b7d
Matvii Oleh <oleh.m97@gmail.com>
2024-11-25 18:25:47 +0200

    feat: ingre component

--- shared/components/shared/orders/order-status.tsx
+++ shared/components/shared/orders/order-status.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { cn } from '@/lib/utils';
+import { cn } from '@/shared/libs/utils';
 import { OrderStatus as IOrderStatus } from '@prisma/client';
 import { Badge } from "lucide-react";
 
